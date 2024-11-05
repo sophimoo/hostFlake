a869634da0f1a777e3208d061bd6a76e24d5d5dc
Sophie <sophimoo@protonmail.com>
2024-11-05 04:57:34 +0100

    idk

--- hosts/nixHomeDesktop/configuration.nix
+++ hosts/nixHomeDesktop/configuration.nix
@@ -199,8 +199,16 @@ in
 
    };
 
-    # Enable OpenGL
-  hardware.opengl = {
+  # Enable OpenGL
+#   hardware.opengl = {
+#     enable = true;
+#     driSupport = true;
+#     driSupport32Bit = true;
+#     extraPackages = with pkgs; [
+#     ];
+#   };
+
+  hardware.graphics = {
     enable = true;
     driSupport = true;
     driSupport32Bit = true;
