f4007c38a9c409d3aee026ae773555c05c6acc3e
Sophie <sophimoo@protonmail.com>
2024-11-09 06:09:47 +0100

    VM update

--- hosts/nixArmVM/home.nix
+++ hosts/nixArmVM/home.nix
@@ -16,7 +16,7 @@
 
   discordOverlay.enable = false;
   # if this is enabled while having already used firefox on the system home-manager will fail due to .mozilla conflict
-  firefox.enable = false;
+  firefox.enable = true;
   textfox.enable = true;
 
   home.username = "${name}";
