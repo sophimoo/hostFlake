d01cfd930d151ae7a081e5855d57056ce7c2b27a
Sophie <sophimoo@protonmail.com>
2024-11-21 06:16:21 +0100

    lf

--- home-manager/programs/lf.nix
+++ home-manager/programs/lf.nix
@@ -20,6 +20,18 @@
       lf = {
         enable = true;
 
+        commands = {
+          dragon-out = ''%${pkgs.xdragon}/bin/xdragon -a -x "$fx"'';
+          editor-open = ''$$EDITOR $f'';
+          mkdir = ''
+          ''${{
+            printf "Directory Name: "
+            read DIR
+            mkdir $DIR
+          }}
+          '';
+        };
+
         settings = {
           drawbox = true;
           hidden = true;
@@ -33,6 +45,7 @@
           &ctpv -s $id
           &ctpvquit $id
         '';
+
       };
 
     };
